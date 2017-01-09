1aa3cfe8272db32af72f662ffb42fff4f101bfa4
czyGod <a549102100@163.com>
2017-01-09 16:40:23 +0800

    "初始化"

---
+++ Demo/Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by 蔡卓越 on 2017/1/9.
+//  Copyright © 2017年 beyondin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo/Demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by 蔡卓越 on 2017/1/9.
+//  Copyright © 2017年 beyondin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
